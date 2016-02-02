f74aa80117d1f76881d1a1721ca2be0d42edf5f4
Nimesh <nimeshg@navantis.com>
2016-02-02 14:00:13 +0530

    added jackson
    tested how to get json to controller

--- src/main/java/com/npg/demo/HelloController.java
+++ src/main/java/com/npg/demo/HelloController.java
@@ -1,12 +1,16 @@
 package com.npg.demo;
 
 import com.npg.demo.model.User;
+import com.npg.demo.request.model.UserRequestModel;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
@@ -18,16 +22,16 @@ public class HelloController {
     @Autowired
     private SessionFactory sessionFactory;
 
-    @RequestMapping("/helloWorld")
-    public String helloWorld(Model model) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        User user = new User();
-        user.setFirstName("Nimesh");
-        user.setLastName("Gunasekara");
-        user.setCreateDate(new Date());
-        session.save(user);
-        session.getTransaction().commit();
+    @RequestMapping(value = "/helloWorld", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public String helloWorld(@RequestBody UserRequestModel user, Model model) {
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        UserRequestModel user = new UserRequestModel();
+//        user.setFirstName("Nimesh");
+//        user.setLastName("Gunasekara");
+//        user.setCreateDate(new Date());
+//        session.save(user);
+//        session.getTransaction().commit();
         model.addAttribute("message", "Hello World!ssss");
         return "helloWorld";
     }
---
+++ src/main/java/com/npg/demo/request/model/UserRequestModel.java
@@ -0,0 +1,25 @@
+package com.npg.demo.request.model;
+
+/**
+ * Created by nimeshg on 2/2/2016.
+ */
+public class UserRequestModel {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
